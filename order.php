5fd531d19e7c3705e4c3b294d14bdcf8387960d9
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-02-21 08:48:57 -0300

    Add files via upload

--- order.php
+++ order.php
@@ -1,5 +1,5 @@
 <?php
-require("includes/common.php");
+require("common.php");
 if (!isset($_SESSION['email'])) {
     header('location: index.php');
 }
@@ -9,23 +9,23 @@ if (!isset($_SESSION['email'])) {
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Confirmation | Life Style Store</title>
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        <link href="bootstrap.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+        <script src="jquery.js"></script>
+        <script src="bootstrap.min.js"></script>
     </head>
     <body>
-        <?php include 'includes/header.php'; ?>
+        <?php include 'header.php'; ?>
         <div class="container-fluid" id="content">
             <div class="col-md-12">
                 <div class="jumbotron">
-                      <h3 align="center">Your order has been placed successfully. Thank you for shopping with us.</h3><hr>
-                      <p align="center">Your order will be delivered in 2 days</p><hr>
-                    <p align="center">Click <a href="products.php">here</a> to purchase any other item.</p>
+                      <h3 align="center">Seu pedido foi feito com sucesso. Obrigado por comprar conosco.</h3><hr>
+                      <p align="center">Seu pedido será entregue em 2 dias</p><hr>
+                    <p align="center">Clique <a href="products.php">aqui</a> para comprar qualquer outro item.</p>
                 </div>
             </div>
         </div>
-        <?php include("includes/footer.php");
+        <?php include("footer.php");
         ?>
     </body>
 </html>
